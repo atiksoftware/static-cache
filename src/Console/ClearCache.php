155b2d2864ba87e2fae046de5aceb62d9aadceca
Atiksoftware Corporation <atiksoftware@gmail.com>
2022-05-02 02:11:54 +0300

    update

--- src/Console/ClearCache.php
+++ src/Console/ClearCache.php
@@ -12,14 +12,14 @@ class ClearCache extends Command
 	 *
 	 * @var string
 	 */
-	protected $signature = 'page-cache:clear {slug? : URL slug of page/directory to delete} {--recursive}';
+	protected $signature = 'static-cache:clear {slug? : URL slug of page/directory to delete. can be line posts*}';
 
 	/**
 	 * The console command description.
 	 *
 	 * @var string
 	 */
-	protected $description = 'Clear (all or part of) the page cache.';
+	protected $description = 'Clear (all or part of) the static page cache.';
 
 	/**
 	 * Execute the console command.
@@ -27,43 +27,10 @@ class ClearCache extends Command
 	public function handle(): void
 	{
 		$cache = $this->laravel->make(Cache::class);
-		$recursive = $this->option('recursive');
 		$slug = $this->argument('slug');
 
-		if (!$slug) {
-			$this->clear($cache);
-		} elseif ($recursive) {
-			$this->clear($cache, $slug);
-		} else {
-			$this->forget($cache, $slug);
-		}
-	}
+		$this->info("Clearing cache by search {$slug} ");
 
-	/**
-	 * Remove the cached file for the given slug.
-	 *
-	 * @param string $slug
-	 */
-	public function forget(Cache $cache, $slug): void
-	{
-		if ($cache->forget($slug)) {
-			$this->info("Page cache cleared for \"{$slug}\"");
-		} else {
-			$this->info("No page cache found for \"{$slug}\"");
-		}
-	}
-
-	/**
-	 * Clear the full page cache.
-	 *
-	 * @param null|string $path
-	 */
-	public function clear(Cache $cache, $path = null): void
-	{
-		if ($cache->clear($path)) {
-			$this->info('Page cache cleared at ' . $cache->getCachePath($path));
-		} else {
-			$this->warn('Page cache not cleared at ' . $cache->getCachePath($path));
-		}
+		$cache->clear($slug, $this);
 	}
 }
